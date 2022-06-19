0961eeba7e378bccc6ec885c206cbb04c3ab528f
Abdullah Yakub <abdullah.yakub@hotmail.com>
2022-06-19 20:44:29 +1000

    test api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [test, setTest] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://api.spaceflightnewsapi.net/v3/articles/')
+      .then(res => res.data)
+      .then(result => setTest(result[0]))
+  })
+
   return (
     <div className="App">
-      nothing
+      <h2>{test.title}</h2>
+      <img src={test.imageUrl} alt='' width='100px' />
     </div>
-  );
+  )
 }
 
 export default App;
