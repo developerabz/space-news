14fc09b207ad14ce4bdf57073332be5d43f7ef57
Abdullah Yakub <abdullah.yakub@hotmail.com>
2022-06-19 23:45:03 +1000

    add api news article cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Card from "./Components/Card";
+import './index.css'
 
 const App = () => {
-  const [test, setTest] = useState([])
+  const [articles, setArticles] = useState([])
 
   useEffect(() => {
     axios
       .get('https://api.spaceflightnewsapi.net/v3/articles/')
       .then(res => res.data)
-      .then(result => setTest(result))
+      .then(result => setArticles(result))
   }, [])
-
+  console.log(articles[0])
   
   
   return (
     <div className="App">
-      <Card test={test}/>
+      <Card articles={articles}/>
     </div>
   )
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,53 @@
-const Card = ({test}) => {
-    const array = test.map(tes => {
+import './card.css'
+
+const Card = ({ articles }) => {
+    const styleImage = {
+        width: '250px',
+        height: '200px',
+        
+    }
+
+    const cards = {
+        width: '500px',
+        height: 'fit-content',
+        border: '2px solid gray',
+        margin: '10px',
+        padding: '10px',
+        borderRadius: '15px',
+        background: 'lightblue',
+    }
+
+    const flex = {
+        display: 'flex',
+        alignItems: 'center',
+    }
+
+    const array = articles.map(article => {
         return (
-            <div>
-                <h2>{tes.title}</h2>
+            <div style={cards} key={article.id}>
+                <a 
+                    href={article.url}
+                    className='link'
+                    target='_blank'
+                >
+                    <div style={flex}>
+                        <div>
+                            <h2>{article.title}</h2>
+                            <span>
+                                {article.newsSite} - {article.updatedAt.slice(0, 10)}
+                            </span>
+                        </div>
+                        <img src={article.imageUrl} alt='' style={styleImage} />
+                    </div>
+                    <p>{article.summary}</p>
+                </a>
             </div>
         )
       })
     return (
-        <main>{array}</main>
+        <main className='main'>
+            {array}
+        </main>
     )
     
 }
